f78aa9a4d5e3ee9a951a12a1b11c4449c29e8d07
mario silva <mario.vinicius.negocios@hotmail.com>
2025-05-16 19:17:03 -0300

    database e outras alterações

---
+++ extra-webpack.config.js
@@ -0,0 +1,14 @@
+const webpack = require('webpack');
+
+module.exports = {
+  resolve: {
+    fallback: {
+      crypto: require.resolve('crypto-browserify'),
+    },
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+    }),
+  ],
+};
